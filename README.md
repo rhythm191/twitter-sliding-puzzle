59ea038964a5ad639d180c22170c0d60c30f4200
rhythm191 <sac2ndg+develop@gmail>
2020-01-24 16:58:20 +0900

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# Twitter Slide Pazzle
+
+Slide pazzle application to use twitter images.
+
+## Development
+
+
+Inside that directory, you can run several commands:
+  yarn dev
+    Starts the development server.
+
+  yarn build
+    Builds the app for production.
+
+  yarn start
+    Runs the built app in production mode.
+
+We suggest that you begin by typing:
+
+  cd with-typescript-app
+  yarn dev
+
+
+### LICENSE
+
+MIT
---
+++ components/Layout.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+
+type Props = {
+  title?: string
+}
+
+const Layout: React.FunctionComponent<Props> = ({
+  children,
+  title = 'Twitter Slide Pazzle',
+}) => (
+  <div>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <header>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>{' '}
+        |{' '}
+        <Link href="/about">
+          <a>About</a>
+        </Link>{' '}
+      </nav>
+    </header>
+    <main>
+      {children}
+    </main>
+    <footer>
+      <hr />
+      <span>Copyright @rhythm191 2020</span>
+    </footer>
+  </div>
+)
+
+export default Layout

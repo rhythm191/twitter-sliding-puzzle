3874dbb37728c49a81c3b5c9f13b6ea8144cf1db
rhythm191 <sac2ndg+develop@gmail>
2020-01-29 20:27:28 +0900

    feat: add commands component test

--- components/__tests__/Commands.test.tsx
+++ components/__tests__/Commands.test.tsx
@@ -4,5 +4,18 @@ import { Commands } from "../Commands";
 
 test("handleinitPieces", () => {
   const handleinitPiecesMock = jest.fn();
-  const command = shallow(<Commands handleinitPieces={handleinitPiecesMock} />);
+  const command = shallow(
+    <Commands
+      handleDebugInit={() => {}}
+      handleinitPieces={handleinitPiecesMock}
+      handleRandom={() => {}}
+    />
+  );
+
+  command
+    .find("button")
+    .at(1)
+    .simulate("click");
+
+  expect(handleinitPiecesMock.mock.calls.length).toBe(1);
 });
--- jest.config.js
+++ jest.config.js
@@ -42,7 +42,11 @@ module.exports = {
   // A path to a module which exports an async function that is triggered once after all test suites
   // globalTeardown: undefined,
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  globals: {
+    "ts-jest": {
+      babelConfig: true,
+    },
+  },
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
   // An array of directory names to be searched recursively up from the requiring module's location
@@ -120,7 +124,9 @@ module.exports = {
   // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
   // timers: "real",
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "/node_modules/"

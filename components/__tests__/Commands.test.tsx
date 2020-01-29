5cc8389b3c7e68729a14c948a4036f25afb63d53
rhythm191 <sac2ndg+develop@gmail>
2020-01-29 21:58:05 +0900

    feat: add Command spec

--- components/__tests__/Commands.test.tsx
+++ components/__tests__/Commands.test.tsx
@@ -2,6 +2,23 @@ import * as React from "react";
 import { shallow } from "enzyme";
 import { Commands } from "../Commands";
 
+test("handleDebugInit", () => {
+  const handleDebugInitMock = jest.fn();
+  const command = shallow(
+    <Commands
+      handleDebugInit={handleDebugInitMock}
+      handleinitPieces={() => {}}
+      handleRandom={() => {}}
+    />
+  );
+
+  const button = command.find("button").at(0);
+  button.simulate("click");
+
+  expect(button.text()).toBe("デバッグ初期化");
+  expect(handleDebugInitMock.mock.calls.length).toBe(1);
+});
+
 test("handleinitPieces", () => {
   const handleinitPiecesMock = jest.fn();
   const command = shallow(
@@ -12,10 +29,26 @@ test("handleinitPieces", () => {
     />
   );
 
-  command
-    .find("button")
-    .at(1)
-    .simulate("click");
+  const button = command.find("button").at(1);
+  button.simulate("click");
 
+  expect(button.text()).toBe("初期化");
   expect(handleinitPiecesMock.mock.calls.length).toBe(1);
+});
+
+test("handleinitPieces", () => {
+  const handleRandomMock = jest.fn();
+  const command = shallow(
+    <Commands
+      handleDebugInit={() => {}}
+      handleinitPieces={() => {}}
+      handleRandom={handleRandomMock}
+    />
+  );
+
+  const button = command.find("button").at(2);
+  button.simulate("click");
+
+  expect(button.text()).toBe("ランダム");
+  expect(handleRandomMock.mock.calls.length).toBe(1);
 });

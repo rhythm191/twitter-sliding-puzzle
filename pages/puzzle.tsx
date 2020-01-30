ec7f9e3d3e3994eea369eda46183ed690bf5e5a9
rhythm191 <sac2ndg+develop@gmail>
2020-01-30 14:55:54 +0900

    firebase initialize

--- pages/puzzle.tsx
+++ pages/puzzle.tsx
@@ -1,19 +1,22 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import Puzzle from "../components/Puzzle";
 import { NextPage } from "next";
 import configureStore from "../store";
 import { Provider } from "react-redux";
+import Commands from "../components/Commands";
 
 const store = configureStore();
 
-const IndexPage: NextPage = () => {
+const PuzzlePage: NextPage = () => {
   return (
     <Provider store={store}>
-      <Layout title="Home | Twitter Sliding Puzzle">
-        <p>Twitterにログインしてください。</p>
+      <Layout title="Puzzle | Twitter Sliding Puzzle">
+        <Commands />
+        <Puzzle />
       </Layout>
     </Provider>
   );
 };
 
-export default IndexPage;
+export default PuzzlePage;

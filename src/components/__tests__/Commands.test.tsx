446253b0435185c79ff69dfca6f10696c4a378cc
rhythm191 <sac2ndg+develop@gmail.com>
2020-05-04 10:49:38 +0900

    fix: command test

--- src/components/__tests__/Commands.test.tsx
+++ src/components/__tests__/Commands.test.tsx
@@ -2,43 +2,52 @@ import * as React from "react";
 import { useDispatch } from "react-redux";
 import { shallow } from "enzyme";
 import { Commands } from "@/components/Commands";
+import * as actions from "@/actions/puzzle";
 
-jest.mock("@/actions/puzzle");
 jest.mock("react-redux");
 const useDispatchMock = useDispatch as jest.Mock;
 
-test("handleDebugInit", () => {
-  const handleDebugInitMock = jest.fn();
-  useDispatchMock.mockReturnValue(handleDebugInitMock);
-  const command = shallow(<Commands />);
+describe("Commands", () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 
-  const button = command.find("button").at(0);
-  button.simulate("click");
+  it("handleDebugInit", () => {
+    const handleDebugInitMock = jest.fn();
+    useDispatchMock.mockReturnValue(handleDebugInitMock);
+    const command = shallow(<Commands />);
 
-  expect(button.text()).toBe("デバッグ初期化");
-  expect(handleDebugInitMock.mock.calls.length).toBe(1);
-});
+    const button = command.find("button").at(0);
+    button.simulate("click");
 
-test("handleinitPieces", () => {
-  const handleinitPiecesMock = jest.fn();
-  useDispatchMock.mockReturnValue(handleinitPiecesMock);
-  const command = shallow(<Commands />);
+    expect(button.text()).toBe("デバッグ初期化");
+    expect(handleDebugInitMock.mock.calls.length).toBe(1);
+    expect(handleDebugInitMock.mock.calls[0][0]).toEqual(actions.debugInit());
+  });
 
-  const button = command.find("button").at(1);
-  button.simulate("click");
+  it("handleinitPieces", () => {
+    const handleinitPiecesMock = jest.fn();
+    useDispatchMock.mockReturnValue(handleinitPiecesMock);
+    const command = shallow(<Commands />);
 
-  expect(button.text()).toBe("初期化");
-  expect(handleinitPiecesMock.mock.calls.length).toBe(1);
-});
+    const button = command.find("button").at(1);
+    button.simulate("click");
 
-test("handleinitPieces", () => {
-  const handleRandomMock = jest.fn();
-  useDispatchMock.mockReturnValue(handleRandomMock);
-  const command = shallow(<Commands />);
+    expect(button.text()).toBe("初期化");
+    expect(handleinitPiecesMock.mock.calls.length).toBe(1);
+    expect(handleinitPiecesMock.mock.calls[0][0]).toEqual(actions.init());
+  });
 
-  const button = command.find("button").at(2);
-  button.simulate("click");
+  it("handleinitPieces", () => {
+    const handleRandomMock = jest.fn();
+    useDispatchMock.mockReturnValue(handleRandomMock);
+    const command = shallow(<Commands />);
 
-  expect(button.text()).toBe("ランダム");
-  expect(handleRandomMock.mock.calls.length).toBe(1);
+    const button = command.find("button").at(2);
+    button.simulate("click");
+
+    expect(button.text()).toBe("ランダム");
+    expect(handleRandomMock.mock.calls.length).toBe(1);
+    expect(handleRandomMock.mock.calls[0][0]).toEqual(actions.setRandom());
+  });
 });
